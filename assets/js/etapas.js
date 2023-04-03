7db72e3c9e0e0b29725ff480fbab870b0fee50b7
tiagoridev <tiagobr.desenvolvedor@gmail.com>
2023-04-03 18:51:54 -0300

    create mobile screen

---
+++ assets/js/etapas.js
@@ -0,0 +1,25 @@
+let candidates = [
+    {
+        post: "REPRESENTANTE DA TURMA",
+        numbers: 2,
+        candidate:[{
+            number: 10,
+            name: "Lisa Simpson",
+            foto: [{url: "lisa.jpg"}]
+        },
+        {
+            number: 15,
+            name: "Bart Simpson",
+            foto: [{url: "bart.jpg"}]
+        },
+        {
+            number: 20,
+            name: "Milhouse",
+            foto: [{url: "milhouse.jpg"}]
+        },
+        ]
+    }
+]
+
+console.log(candidates[0].candidate[2])
+ 
